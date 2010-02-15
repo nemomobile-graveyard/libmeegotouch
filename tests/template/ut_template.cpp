a0f70773cf56ac59076922fc0229f73789c0b04d
Aki Koskinen <aki.koskinen@nokia.com>
2010-02-15 16:39:54 +0200

    Changes: ut_template
    RevBy: TrustMe

--- tests/template/ut_template.cpp
+++ tests/template/ut_template.cpp
@@ -17,32 +17,31 @@
 **
 ****************************************************************************/
 
-#include <QObject>
-#include <QGraphicsSceneMouseEvent>
+#include "ut_mkutclassname.h"
+
 
 mkutclassinclude
 mkutclassinclude_p
 
-#include "ut_mkutclassname.h"
-
-void Ut_MkUtClassName::init()
-{
-    m_subject = new MkUtClassName();
-}
-
-void Ut_MkUtClassName::cleanup()
-{
-    delete m_subject;
-    m_subject = 0;
-}
+#include <QtTest/QtTest>
 
 void Ut_MkUtClassName::initTestCase()
 {
-    QSKIP("incomplete", SkipSingle);   // remove this when you've finished
 }
 
 void Ut_MkUtClassName::cleanupTestCase()
 {
 }
 
-QTEST_MAIN(Ut_MkUtClassName)
+void Ut_MkUtClassName::init()
+{
+    m_subject = new MkUtClassName;
+}
+
+void Ut_MkUtClassName::cleanup()
+{
+    delete m_subject;
+    m_subject = NULL;
+}
+
+QTEST_APPLESS_MAIN(Ut_MkUtClassName)
--- tests/template/ut_template.pro
+++ tests/template/ut_template.pro
@@ -1,33 +1,12 @@
 include(../common_top.pri)
 TARGET = ut_mkutclassname
 
-TEST_SOURCES = \
-    $$DUISRCDIR/mkutclassname.cpp \
-
-# unit test and unit
 SOURCES += \
     ut_mkutclassname.cpp \
-    $$TEST_SOURCES \ 
+    $$DUISRCDIR/mkutclassname.cpp
 
-# base classes
-SOURCES += \
-    mkutbasesources
-
-# service classes
-SOURCES += \
-    $$STUBSDIR/stubbase.cpp \
-
-# unit test and unit
 HEADERS += \
     ut_mkutclassname.h \
-    $$DUISRCDIR/mkutclassname.h \
-
-# base classes
-HEADERS += \
-    mkutbaseheaders
-
-# service classes
-HEADERS += \
-    mkutserviceheaders
+    $$DUISRCDIR/mkutclassname.h
 
 include(../common_bot.pri)
