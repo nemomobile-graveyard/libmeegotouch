nemomobile-graveyard/libmeegotouch

commit c70d8e379190a1e4f641c6bc4f7a8dd4bf436985

Fixes: NB#156583 - Problem with DUI window management?
RevBy: TrustMe
Details: Don't switch the active window on WindowDeactivate event

src/core/duicomponentdata.cpp
@@ -741,7 +741,11 @@ void DuiComponentData::setActiveWindow(DuiWindow *w)
     if (!gDuiComponentDataPrivate) {
         qFatal("DuiComponentData::setActiveWindow() - DuiComponentData instance not yet created.");
     }
+
     DuiWindow *activeWindow = DuiComponentData::activeWindow();
+    if (activeWindow == w)
+        return;
+
     if (activeWindow) {
         QObject::disconnect(activeWindow,
                             SIGNAL(orientationAngleChanged(Dui::OrientationAngle)),

src/widgets/duiwindow.cpp
@@ -590,20 +590,22 @@ void DuiWindow::paintEvent(QPaintEvent *event)
 bool DuiWindow::event(QEvent *event)
 {
     Q_D(DuiWindow);
-    if (event->type() == QEvent::Show || event->type() == QEvent::WindowActivate) {
+
+    if (event->type() == QEvent::Show || event->type() == QEvent::WindowActivate)
         DuiComponentData::setActiveWindow(this);
+
+    if (event->type() == QEvent::Close)
+#if defined DUI_USE_OPENGL
+        if (!DuiApplication::softwareRendering()) {
+            DuiGLES2Renderer::destroy(d->glWidget);
+#endif
+        if (DuiComponentData::windows().size() > 1)
+            DuiComponentData::setActiveWindow(DuiComponentData::windows().at(1));
     }
 
-#if defined DUI_USE_OPENGL
-    if (event->type() == QEvent::Close && !DuiApplication::softwareRendering()) {
-        DuiGLES2Renderer::destroy(d->glWidget);
-    }
-#endif
     if (event->type() == QEvent::Close || event->type() == QEvent::WindowDeactivate) {
         DuiOnDisplayChangeEvent ev(false, sceneRect());
         onDisplayChangeEvent(&ev);
-        if (DuiComponentData::windows().size() > 1)
-            DuiComponentData::setActiveWindow(DuiComponentData::windows().at(1));
     }
 
     if (QEvent::KeyPress == event->type()) {

commit 39c12f0d61a54aa431f8e8a662ead5e2fa4fd215

Fixes: NB#160373, DuiInputMethodState updating
RevBy: Dominik Kapusta
Details: this reverts an optimization made as part of
c70d8e379190a1e4f641c6bc4f7a8dd4bf436985, which caused orientation change
signal connection and orientation updating not to be performed for some
windows because there is always an active window as long as window list is
not empty, even if setActiveWindow() has not been called before.

src/core/duicomponentdata.cpp
@@ -743,8 +743,6 @@ void DuiComponentData::setActiveWindow(DuiWindow *w)
     }
 
     DuiWindow *activeWindow = DuiComponentData::activeWindow();
-    if (activeWindow == w)
-        return;
 
     if (activeWindow) {
         QObject::disconnect(activeWindow,