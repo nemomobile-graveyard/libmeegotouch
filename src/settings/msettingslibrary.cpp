ade52530fb2c84d9eb9d464e337c5d532d60abda
Tomas Junnonen <tomas.junnonen@nokia.com>
2010-04-22 16:22:56 +0300

    Changes: Registering the settings library once again
    RevBy: TrustMe
    Details: Apparently the library must be registered even though it doesn't have any
    CSS.

--- src/settings/msettingslibrary.cpp
+++ src/settings/msettingslibrary.cpp
@@ -1,9 +1,6 @@
 // initialize the msettings library for styles etc. here
 
-/* Currently the settings library doesn't contain any styles or views,
-   registering is therefore disabled.
 #include "mlibrary.h"
 
 M_LIBRARY
-*/
 
