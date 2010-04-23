d3849623b695936957bb7ed845d792a73e5b77ce
Daniel d'Andrada <daniel.dandrada@nokia.com>
2010-04-23 09:37:06 +0300

    New: First part of the "MeeGo Touch Tutorial"
    RevBy: TrustMe
    Details: Still lot's of stuff to be written.

---
+++ examples/tutorial/tutorial_main_step_03.cpp
@@ -0,0 +1,19 @@
+#include <MApplication>
+#include <MApplicationWindow>
+#include <MApplicationPage>
+#include <MLabel>
+
+int main(int argc, char **argv)
+{
+    MApplication app(argc, argv);
+    MApplicationWindow window;
+    MApplicationPage page;
+
+    page.setTitle("My First Page");
+    page.setCentralWidget(new MLabel("Hello World!"));
+    page.appear();
+
+    window.show();
+
+    return app.exec();
+}
