3a26407781a1806463e90ec43824b0d57fd5e7c0
Sachin Kundu <ext-sachin.kundu@nokia.com>
2010-04-28 09:36:51 +0300

    Revert "Changes: Fix for OOP extensions not visible. Also, handles the removed visibilityChanged(bool) signal."
    Still breaks unit test build. giving up
    This reverts commit 8eafc33dedec829763de0ff19e1b30b869a98c68.

--- src/extensions/applicationextension/mextensionrunner.cpp
+++ src/extensions/applicationextension/mextensionrunner.cpp
@@ -223,7 +223,7 @@ void MExtensionRunner::messageReceived(const MAppletMessage &message)
                 // Visible area rectangle is irrelevant when sending
                 // on or off display events.
                 QRectF dummyRectangle;
-                emit visibilityChanged();
+                emit visibilityChanged(m->isVisible());
                 visible = m->isVisible();
                 if (visible) {
                     MOnDisplayChangeEvent event(MOnDisplayChangeEvent::FullyOnDisplay, dummyRectangle);
