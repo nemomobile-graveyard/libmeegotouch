8d334aab935c3497a9c165f253fb49f2baa67d26
Armin Berres <armin.berres@basyskom.de>
2010-05-10 13:15:38 +0200

    Changes: make sure timedemo fails if no or a broken window manager is running
    RevBy: TrustMe
    
    Details:
    If no windowmanager is running or a broken window manager is running pages
    cannot be activated properly. This commit introduces a timer which makes
    sure the page has really been activated and terminates the application with
    a warning otherwise.

--- demos/widgetsgallery/timedemobenchmark.cpp
+++ demos/widgetsgallery/timedemobenchmark.cpp
@@ -1,10 +1,15 @@
 #include "timedemobenchmark.h"
 
+#include <QTimer>
+#include <MApplicationPage>
+
 TimedemoBenchmark::TimedemoBenchmark(MApplicationPage *applicationPage, Timedemo *timedemo)
     : applicationPage(applicationPage)
     , timedemo(timedemo)
     , _type("common")
 {
+    verifyAppearanceTimer = new QTimer(this);
+    connect(verifyAppearanceTimer, SIGNAL(timeout()), this, SLOT(terminateIfNotAppeared()));
 }
 
 QString TimedemoBenchmark::type() {
@@ -13,4 +18,14 @@ QString TimedemoBenchmark::type() {
 
 void TimedemoBenchmark::setType(const QString& type) {
     _type = type;
+}
+
+void TimedemoBenchmark::terminateIfNotAppeared() {
+    verifyAppearanceTimer->stop();
+    if (applicationPage->isActiveWindow()) {
+        return;
+    } else {
+        qCritical("Could not activate application page. Is a window manager running?");
+        exit(EXIT_FAILURE);
+    }
 }
