nemomobile-graveyard/libmeegotouch

commit 1ca3769d4eae045ff3c4c0606b1345c1f1afee7f

Changes: Added paintOffset property to MWidget
RevBy: John Tapsell
Details: This property allows to offset the widget display position without
effecting for example the layout.

src/corelib/widgets/core/mwidget.cpp
@@ -455,3 +455,12 @@ void MWidget::setVisible(bool visible)
         QGraphicsWidget::setVisible(visible);
 }
 
+QPointF MWidget::paintOffset() const
+{
+    return QPointF(transform().dx(), transform().dy());
+}
+
+void MWidget::setPaintOffset(const QPointF &newOffset)
+{
+    setTransform(QTransform::fromTranslate(newOffset.x(), newOffset.y()));
+}

src/corelib/widgets/core/mwidget.h
@@ -47,6 +47,7 @@ class M_EXPORT MWidget : public QGraphicsWidget
     Q_PROPERTY(QSizePolicy sizePolicy READ sizePolicy WRITE setSizePolicy)
     Q_PROPERTY(bool selected READ isSelected WRITE setSelected)
     Q_PROPERTY(bool onDisplay READ isOnDisplay)
+    Q_PROPERTY(QPointF paintOffset READ paintOffset WRITE setPaintOffset)
 
 public:
     MWidget(QGraphicsItem *parent = 0);
@@ -88,6 +89,31 @@ public:
     * \sa setSelected(bool)
     */
     bool isSelected() const;
+
+    /*!
+     Returns the offset by which the widget will be transformed when painted.
+
+     This convenience function is equivalent to querying the horizontal and
+     vertical translation factors of the widgets's transform().
+
+     \sa setPaintOffset()
+     */
+    QPointF paintOffset() const;
+
+    /*!
+     Sets the offset by which the widget will be transformed when painted.
+
+     This convenience function is equivalent to calling setTransform on the widget,
+     supplying a QTransform that shifts the widget by \a offset.
+
+     The paint offset allows for a widget to appear as if in a position
+     different than that of the widget's pos() property. This allows for example
+     to animate the widget's apparent location using a QPropertyAnimation
+     without affecting layouting or the layout affecting the animation.
+
+     \sa paintOffset()
+     */
+    void setPaintOffset(const QPointF & offset);
 
     /*! \reimp
      *   We reimplement these to distinguish between the user hiding items

commit 414ab662ce005f417b54bd7715b1d608acb9e9ae

Changes: fix build by adding stubs to mwidget_stub.h
RevBy: Torsten Rahn

Details:

fixes build broken by 1ca3769d4eae045ff3c4c0606b1345c1f1afee7f

tests/stubs/mwidget_stub.h
@@ -35,6 +35,8 @@ class MWidgetStub : public StubBase {
   virtual MSceneManager * sceneManager() const;
   virtual bool isOnDisplay() const;
   virtual bool isSelected() const;
+  virtual QPointF paintOffset() const;
+  virtual void setPaintOffset(const QPointF & offset);
   virtual void setVisible(bool visible);
   virtual void setObjectName(const QString &name);
   virtual void setSelected(bool selected);
@@ -91,6 +93,17 @@ bool MWidgetStub::isOnDisplay() const {
 bool MWidgetStub::isSelected() const {
   stubMethodEntered("isSelected");
   return stubReturnValue<bool>("isSelected");
+}
+
+QPointF MWidgetStub::paintOffset() const {
+  stubMethodEntered("paintOffset");
+  return stubReturnValue<QPointF>("paintOffset");
+}
+
+void MWidgetStub::setPaintOffset(const QPointF & offset) {
+  QList<ParameterBase*> params;
+  params.append( new Parameter<const QPointF & >(offset));
+  stubMethodEntered("setPaintOffset",params);
 }
 
 void MWidgetStub::setVisible(bool visible) {
@@ -251,6 +264,14 @@ bool MWidget::isOnDisplay() const {
 
 bool MWidget::isSelected() const {
   return gMWidgetStub->isSelected();
+}
+
+QPointF MWidget::paintOffset() const {
+    return gMWidgetStub->paintOffset();
+}
+
+void MWidget::setPaintOffset(const QPointF & offset) {
+    gMWidgetStub->setPaintOffset(offset);
 }
 
 void MWidget::setVisible(bool visible) {