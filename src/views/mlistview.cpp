737c351549a8a330b9a8fe380ff1365871693d1d
Stanislav Ionascu <stanislav.ionascu@nokia.com>
2010-06-18 16:19:53 +0300

    Fixes: NB#175429 - MList crashes after a modelReset if group headers are shown and one group header is removed
    RevBy: Sergiy Dubovik
    
    Details: The problem was that the items are going to be relayouted after the first paint event. So we clear
    first and last visible rows until the items are relayouted. But the items are not actually removed from the
    screen.

--- src/views/mlistview.cpp
+++ src/views/mlistview.cpp
@@ -199,7 +199,8 @@ void MListView::drawForeground(QPainter *painter, const QStyleOptionGraphicsItem
 
 void MListView::drawBackground(QPainter *painter, const QStyleOptionGraphicsItem *option) const
 {
-    d_ptr->drawSeparators(painter, option);
+    if (d_ptr->hseparatorHeight > 0 && !d_ptr->clearVisibleOnRelayout)
+        d_ptr->drawSeparators(painter, option);
 }
 
 void MListView::dataChanged(const QModelIndex &topLeft, const QModelIndex &bottomRight)
