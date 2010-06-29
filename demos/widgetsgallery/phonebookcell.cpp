nemomobile-graveyard/libmeegotouch

commit 63dd8f8ecddc933907399df651e74243e3e972b8

fix coverity id 822

demos/widgetsgallery/phonebookmodel.cpp
@@ -350,7 +350,10 @@ void PhoneBookImageLoader::notifyModel(const QModelIndex &index)
                 sortModel = filterModel;
             }
         }
-        phoneBookModel->thumbnailWasLoaded(sortModel->mapToSource(realIndex));
+        else if ( sortModel != NULL )
+        {
+            phoneBookModel->thumbnailWasLoaded(sortModel->mapToSource(realIndex));
+        }
     }
 #endif
 }

commit 69fba5dd8080bcf4fb8612086e19044ef7350be5

Fixes: NB#176909 - spinners in widgetsgallery list are not replaced with images anymore
RevBy: Armin, Jörgen

Details: Fixes the issue commit 63dd8f8ecddc933907399df651e74243e3e972b8 introduced.
Some improved spinner->image replacement code for the phone book cell class.

demos/widgetsgallery/phonebookcell.cpp
@@ -55,7 +55,11 @@ MLayout *PhoneBookCell::createLayout()
     landscapePolicy->setSpacing(0);
 
     // add to layout
-    landscapePolicy->addItem(spinnerWidget(), 0, 0, 3, 1, Qt::AlignLeft | Qt::AlignVCenter);
+    if (!imageWidget()->pixmap())
+        landscapePolicy->addItem(spinnerWidget(), 0, 0, 3, 1, Qt::AlignLeft | Qt::AlignVCenter);
+    else
+        landscapePolicy->addItem(imageWidget(), 0, 0, 3, 1, Qt::AlignLeft | Qt::AlignVCenter);
+
     landscapePolicy->addItem(landscapeTitleLabelWidget(), 0, 1, Qt::AlignLeft | Qt::AlignTop);
     landscapePolicy->addItem(subtitleLabelWidget(), 1, 1, Qt::AlignLeft | Qt::AlignBottom);
     landscapePolicy->addItem(new QGraphicsWidget(this), 2, 1);
@@ -63,7 +67,12 @@ MLayout *PhoneBookCell::createLayout()
     portraitPolicy = new MLinearLayoutPolicy(layout, Qt::Horizontal);
     portraitPolicy->setContentsMargins(0, 0, 0, 0);
     portraitPolicy->setSpacing(0);
-    portraitPolicy->addItem(spinnerWidget(), Qt::AlignLeft | Qt::AlignVCenter);
+
+    if (!imageWidget()->pixmap())
+        portraitPolicy->addItem(spinnerWidget(), Qt::AlignLeft | Qt::AlignVCenter);
+    else
+        portraitPolicy->addItem(imageWidget(), Qt::AlignLeft | Qt::AlignVCenter);
+
     portraitPolicy->addItem(portraitTitleLabelWidget(), Qt::AlignLeft | Qt::AlignVCenter);
 
     layout->setPortraitPolicy(portraitPolicy);

demos/widgetsgallery/phonebookmodel.cpp
@@ -350,8 +350,7 @@ void PhoneBookImageLoader::notifyModel(const QModelIndex &index)
                 sortModel = filterModel;
             }
         }
-        else if ( sortModel != NULL )
-        {
+        if (phoneBookModel != NULL && sortModel != NULL) {
             phoneBookModel->thumbnailWasLoaded(sortModel->mapToSource(realIndex));
         }
     }