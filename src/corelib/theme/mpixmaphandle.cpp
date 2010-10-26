nemomobile-graveyard/libmeegotouch

commit 4233919167eccee31ea8d673475e4e975525acda

Changes: Modifying building scripts for Mac.
RevBy: TrustMe

src/corelib/theme/mpixmaphandle.cpp
@@ -8,6 +8,6 @@ MPixmapHandle::MPixmapHandle() :
 
 bool MPixmapHandle::isValid() const
 {
-    return (!size.isEmpty() && eglHandle && !shmHandle.isEmpty()) ^ xHandle;
+    return (!size.isEmpty() && eglHandle && !shmHandle.isEmpty()) ^ reinterpret_cast<unsigned long>(xHandle);
 }
 

tools/build-widgetsgallery.xml
@@ -1,13 +1,12 @@
 <?xml version="1.0"?>
 <project name="widgetsgallery">
     <property name="build.root" value="../../.."/>
-    <property name="build.dir" value="${build.root}/duibuild"/>
+    <property name="build.dir" value="${build.root}/meegotouch-build"/>
     <property name="dist.dir" value="${build.root}/widgetsgallery"/>
-    <property name="libdui.dir" value="${build.dir}/libdui"/>
-    <property name="theme.dir" value="${build.dir}/libdui"/>
+    <property name="libdui.dir" value=".."/>
     <property name="widgetsgallery.dir" value="${libdui.dir}/demos/widgetsgallery"/>
     <property name="git" value="/opt/local/bin/git"/>
-    <property name="macdeployqt" value="/usr/bin/macdeployqt"/>
+    <property name="macdeployqt" value="macdeployqt"/>
     <property name="local.frameworks.dir" value="/Users/dubik/Library/Frameworks"/>
 
     <target name="dmg" depends="create-dmg">
@@ -16,6 +15,7 @@
     <target name="init">
         <mkdir dir="${build.dir}"/>
         <mkdir dir="${dist.dir}"/>
+        <mkdir dir="${libdui.dir}"/>
         <delete includeemptydirs="true" failonerror="false">
             <fileset dir="${widgetsgallery.dir}/widgetsgallery.app"/>
         </delete>
@@ -36,13 +36,7 @@
         </exec>
     </target>
 
-    <target name="qmake" depends="configure">
-        <exec executable="qmake" dir="${libdui.dir}">
-            <arg line="-spec macx-g++ -r"/>
-        </exec>
-    </target>
-
-    <target name="compile" depends="qmake">
+    <target name="compile" depends="configure">
         <exec executable="make" failonerror="yes" dir="${libdui.dir}">
             <arg line="-j 2"/>
         </exec>

tools/mtheme.pmdoc/01base-contents.xml
@@ -1,1 +1,1 @@
-<pkg-contents spec="1.12"/>
+<pkg-contents spec="1.12"><f n="base" o="" g="" p="0" pt="/Users/dubik/Projects/DirectUI/duitheme/base" m="false" t="file"/></pkg-contents>

tools/mtheme.pmdoc/01base.xml
@@ -1,1 +1,1 @@
-<pkgref spec="1.12" uuid="AB82375F-4E9E-40AB-B4FD-A78E7C05D0BA"><config><identifier>com.nokia.meegotouch.theme.meegotheme.base.pkg</identifier><version>1</version><description></description><post-install type="none"/><requireAuthorization/><installFrom relative="true">../../duitheme/base</installFrom><installTo mod="true">/usr/share/themes/base</installTo><flags><followSymbolicLinks/></flags><packageStore type="internal"></packageStore><mod>parent</mod><mod>installFrom.isRelativeType</mod><mod>installTo.path</mod><mod>installTo</mod><mod>identifier</mod></config></pkgref>
+<pkgref spec="1.12" uuid="AB82375F-4E9E-40AB-B4FD-A78E7C05D0BA"><config><identifier>com.nokia.meegotouch.theme.meegotheme.base.pkg</identifier><version>1</version><description></description><post-install type="none"/><requireAuthorization/><installFrom relative="true" mod="true">../../meegotouch-theme/base</installFrom><installTo mod="true">/usr/share/themes/base</installTo><flags><followSymbolicLinks/></flags><packageStore type="internal"></packageStore><mod>installFrom.isRelativeType</mod><mod>installTo.path</mod><mod>installFrom.path</mod><mod>parent</mod><mod>installTo</mod><mod>identifier</mod></config><contents><file-list>01base-contents.xml</file-list><filter>/CVS$</filter><filter>/\.svn$</filter><filter>/\.cvsignore$</filter><filter>/\.cvspass$</filter><filter>/\.DS_Store$</filter></contents></pkgref>

tools/mtheme.pmdoc/02devel-contents.xml
@@ -1,1 +1,0 @@
-<pkg-contents spec="1.12"/>

tools/mtheme.pmdoc/02devel.xml
@@ -1,1 +1,0 @@
-<pkgref spec="1.12" uuid="F0F41405-229A-4E5A-9B90-8924F76DBD59"><config><identifier>com.nokia.meegotouch.theme.meegotheme.devel.pkg</identifier><version>1</version><description></description><post-install type="none"/><requireAuthorization/><installFrom relative="true">../../duitheme/devel</installFrom><installTo mod="true">/usr/share/themes/devel</installTo><flags><followSymbolicLinks/></flags><packageStore type="internal"></packageStore><mod>parent</mod><mod>installFrom.isRelativeType</mod><mod>installTo.path</mod><mod>installTo</mod><mod>identifier</mod></config></pkgref>

tools/mtheme.pmdoc/02plankton-contents.xml
@@ -1,0 +1,1 @@
+<pkg-contents spec="1.12"><f n="plankton" o="" g="" p="0" pt="/Users/dubik/Projects/DirectUI/duitheme/plankton" m="false" t="file"/></pkg-contents>

tools/mtheme.pmdoc/02plankton.xml
@@ -1,0 +1,1 @@
+<pkgref spec="1.12" uuid="31BCCE94-FE3D-4B53-B12B-13EE36C322F1"><config><identifier>com.nokia.meegotouch.theme.meegotheme.plankton.pkg</identifier><version>1</version><description></description><post-install type="none"/><requireAuthorization/><installFrom relative="true" mod="true">../../meegotouch-theme/plankton</installFrom><installTo mod="true">/usr/share/themes/plankton</installTo><flags><followSymbolicLinks/></flags><packageStore type="internal"></packageStore><mod>installFrom.isRelativeType</mod><mod>installTo.path</mod><mod>installFrom.path</mod><mod>parent</mod><mod>installTo</mod><mod>identifier</mod></config><contents><file-list>02plankton-contents.xml</file-list><filter>/CVS$</filter><filter>/\.svn$</filter><filter>/\.cvsignore$</filter><filter>/\.cvspass$</filter><filter>/\.DS_Store$</filter></contents></pkgref>

commit 0b0ab7d74fecebdff5a4c49c68e92973b2d70e2e

Changes: Unbreak build caused by 4233919167eccee3
RevBy: Holger

src/corelib/theme/mpixmaphandle.cpp
@@ -8,6 +8,6 @@ MPixmapHandle::MPixmapHandle() :
 
 bool MPixmapHandle::isValid() const
 {
-    return (!size.isEmpty() && eglHandle && !shmHandle.isEmpty()) ^ reinterpret_cast<unsigned long>(xHandle);
+    return (!size.isEmpty() && eglHandle && !shmHandle.isEmpty()) ^ (unsigned long)xHandle;
 }
 