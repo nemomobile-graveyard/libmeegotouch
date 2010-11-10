4f98e0533a02dc2ca3ee8a30d51e51b646869988
Viacheslav Sobolev <ext-viacheslav.sobolev@nokia.com>
2010-11-10 11:50:19 +0200

    Changes: use high resolution timestamp for Unix
    RevBy: Ning Chi, Holger Schröder
    Details: QTime provides milliseconds only, it
    is not enough for some measurements

--- src/corelib/core/mtimestamp.cpp
+++ src/corelib/core/mtimestamp.cpp
@@ -20,11 +20,31 @@
 #include "mtimestamp.h"
 
 #include "mdebug.h"
+#ifdef unix
+#include <time.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#else
 #include <QtCore/QTime>
+#endif
 
 void M_CORE_EXPORT mTimestampStream(const QString &module, const QString &file, const QString &scope, const QString &msg)
 {
+#ifdef unix
+    static const QString format("%1.%2 MARK|%3|%4|%5");
+    static const QChar fill('0');
+    QString output;
+    struct timeval tv;
+
+    gettimeofday(&tv, 0);
+
+    output = format.arg(tv.tv_sec).arg(tv.tv_usec, 6, 10, fill).arg(file).arg(scope).arg(msg);
+    mWarning(module) << output;
+#else
     static const QString format("MARK|%3|%4|%5");
     QString output = format.arg(file).arg(scope).arg(msg);
     mWarning(module) << QTime::currentTime() << output;
+#endif
 }
