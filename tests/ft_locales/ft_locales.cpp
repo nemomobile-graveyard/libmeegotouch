027b9cdc40d72af553826fee2b4430b8c543bdb5
Mike FABIAN <mike.fabian@basyskom.de>
2010-11-25 12:29:07 +0100

    Changes: fix unit text for language endonym
    RevBy: Berthold Krevert, Adrián Yanes
    
    Details:
    
    Language endonym for zh_CN is now “中文 (中国大陆)” instead of
    “中文（简体）”, this has been changed in commit
    
        c134d6e357fa95cef17cac373e333857fb0c8be0

--- tests/ft_locales/ft_locales.cpp
+++ tests/ft_locales/ft_locales.cpp
@@ -609,7 +609,7 @@ void Ft_Locales::testMLocaleLanguageEndonym_data()
             << QString("日本語");
     QTest::newRow("zh_CN")
             << QString("zh_CN")
-            << QString("中文（简体）");
+            << QString("中文 (中国大陆)");
 }
 
 void Ft_Locales::testMLocaleLanguageEndonym()
