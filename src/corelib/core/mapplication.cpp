90cbcc8a618c63fd3a612d1a59002d9540d4ec8a
Jussi Lind <jussi.lind@nokia.com>
2010-12-08 13:54:55 +0200

    Fixes: NB#203650 - displayEntered() not emitted when switching between two windows in the same app
    RevBy: Antti Kervinen
    
    Details: Listen only to synthetic visibility events if mcompositor is running.

--- src/corelib/core/mapplication.cpp
+++ src/corelib/core/mapplication.cpp
@@ -405,36 +405,35 @@ bool MApplication::x11EventFilter(XEvent *event)
 
 void MApplicationPrivate::handleXVisibilityEvent(XVisibilityEvent *xevent)
 {
-    switch (xevent->state) {
-    case VisibilityFullyObscured:
-        // Listen only to synthetic events by compositor
-        if (xevent->send_event)
-        {
-            MWindow * window = MApplicationPrivate::windowForId(xevent->window);
+    static const bool wmRunning = MComponentData::isMeeGoWindowManagerRunning();
+
+    // Listen only to synthetic events if window
+    // manager is running.
+    if (xevent->send_event || !wmRunning) {
+
+        MWindow * window = MApplicationPrivate::windowForId(xevent->window);
+
+        switch (xevent->state) {
+        case VisibilityFullyObscured:
             if (window) {
                 window->d_ptr->fullyObscured = true;
                 if (!window->d_ptr->visibleInSwitcher) {
                     setWindowVisibility(window, false);
                 }
             }
-        }
-        break;
+            break;
 
-        // Always listen to these events, because if compositor is not running
-        // we are not getting any synthetic events at all and the window would never get
-        // the display entered signal.
-    case VisibilityUnobscured:
-    case VisibilityPartiallyObscured:
-        {
-            MWindow * window = MApplicationPrivate::windowForId(xevent->window);
+        case VisibilityUnobscured:
+        case VisibilityPartiallyObscured:
             if (window) {
                 window->d_ptr->fullyObscured = false;
                 setWindowVisibility(window, true);
             }
+            break;
+
+        default:
+            break;
         }
-        break;
-    default:
-        break;
     }
 }
 
