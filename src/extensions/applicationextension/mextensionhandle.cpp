nemomobile-graveyard/libmeegotouch

commit f891042021f2593d74c33de175f2c76077355649

Fixes: NB#208330 - meegotouchhome process causing high wakeups when device is in idle state
RevBy: Vesa Halttunen, Pauli Lehtinen

src/extensions/applicationextension/mextensionhandle.cpp
@@ -57,6 +57,10 @@
 #include <MAction>
 #include <mobjectmenu.h>
 
+#ifdef HAVE_CONTEXTSUBSCRIBER
+#include "contextproperty.h"
+#endif
+
 #include "mwidgetcreator.h"
 M_REGISTER_WIDGET(MExtensionHandle)
 
@@ -66,12 +70,18 @@ const QString MExtensionHandlePrivate::PACKAGE_MANAGER_DBUS_PATH = "/com/nokia/p
 //! D-Bus interface of the Package Manager
 const QString MExtensionHandlePrivate::PACKAGE_MANAGER_DBUS_INTERFACE = "com.nokia.package_manager";
 
+//! Interval for checking that the extension is alive
+static const uint ALIVE_TIMER_INTERVAL = 5000;
+
 MExtensionHandlePrivate::MExtensionHandlePrivate() :
     restartCount(0),
     aliveResponseTimeout(3000),
     runnerConnectionTimeout(20000),
     applicationVisible(true),
     remoteActions(),
+#ifdef HAVE_CONTEXTSUBSCRIBER
+    screenBlankProperty(new ContextProperty("Session.State", this)),
+#endif
     q_ptr(NULL)
 {
     communicationTimer.setSingleShot(true);
@@ -116,29 +126,20 @@ MExtensionHandle::MExtensionHandle(QGraphicsItem *parent) :
     MWidgetController(new MExtensionHandleModel, parent),
     d_ptr(new MExtensionHandlePrivate())
 {
-    Q_D(MExtensionHandle);
-
-    if (d)
-        d->q_ptr = this;
-
-    // TODO: FIXME - this needs to have the scene specified,
-    // temporarily uses currently active MWindow's scene.
-    connect(MApplication::activeWindow(),
-            SIGNAL(orientationChanged(M::Orientation)),
-            this, SLOT(orientationEvent(M::Orientation)));
-
-    connect(this, SIGNAL(visibleChanged()), this, SLOT(visibilityChanged()));
-
-    // Configure the timers
-    connect(&d->aliveTimer, SIGNAL(timeout()), this, SLOT(sendAliveMessageRequest()));
-    connect(&d->communicationTimer, SIGNAL(timeout()), this, SLOT(communicationTimerTimeout()));
+    initialize();
 }
 
 MExtensionHandle::MExtensionHandle(MExtensionHandlePrivate *dd, MExtensionHandleModel *model, QGraphicsItem *parent) :
     MWidgetController(model, parent),
     d_ptr(dd)
 {
+    initialize();
+}
+
+void MExtensionHandle::initialize()
+{
     Q_D(MExtensionHandle);
+
     if (d)
         d->q_ptr = this;
 
@@ -153,11 +154,34 @@ MExtensionHandle::MExtensionHandle(MExtensionHandlePrivate *dd, MExtensionHandle
     // Configure the timers
     connect(&d->aliveTimer, SIGNAL(timeout()), this, SLOT(sendAliveMessageRequest()));
     connect(&d->communicationTimer, SIGNAL(timeout()), this, SLOT(communicationTimerTimeout()));
+
+#ifdef HAVE_CONTEXTSUBSCRIBER
+    connect(d->screenBlankProperty, SIGNAL(valueChanged()), this, SLOT(updateDisplayState()));
+#endif
 }
 
 MExtensionHandle::~MExtensionHandle()
 {
     kill();
+}
+
+void MExtensionHandle::updateDisplayState()
+{
+    Q_D(MExtensionHandle);
+
+#ifdef HAVE_CONTEXTSUBSCRIBER
+    if(state() == MExtensionHandleModel::RUNNING) {
+        bool blanked = d->screenBlankProperty->value().toString() == "blanked";
+
+        if(blanked && d->aliveTimer.isActive()) {
+            d->aliveTimer.stop();
+            d->communicationTimer.stop();
+        } else if(!d->aliveTimer.isActive()) {
+            d->aliveTimer.start(ALIVE_TIMER_INTERVAL);
+            sendAliveMessageRequest();
+        }
+    }
+#endif
 }
 
 void MExtensionHandle::initPlaceHolder(const QString &packageName, const QString &installationError)
@@ -279,9 +303,12 @@ void MExtensionHandle::connectionEstablished()
     // Set the state to running
     model()->setCurrentState(MExtensionHandleModel::RUNNING);
 
-    // Send alive request every 5 seconds. Send the first alive request immediately.
-    d->aliveTimer.start(5000);
+    // Send alive requests at certain interval. Send the first alive request immediately.
+    d->aliveTimer.start(ALIVE_TIMER_INTERVAL);
     sendAliveMessageRequest();
+
+    // Set the current display state
+    updateDisplayState();
 
     // Send the visibility changed message to the runner
     d->visibilityChanged();

src/extensions/applicationextension/mextensionhandle.h
@@ -242,6 +242,11 @@ protected slots:
      */
     void remoteActionTriggered();
 
+    /*!
+     * \brief Stops or starts the alive timer depending on the display blanking state
+     */
+    void updateDisplayState();
+
 signals:
     /*!
      * \brief Signals that a pixmap has been taken into use by the runner
@@ -255,6 +260,9 @@ signals:
 
 private:
     Q_DECLARE_PRIVATE(MExtensionHandle)
+
+    //! Initializes the object
+    void initialize();
 
     //! Sets the state to BROKEN and restarts if this is the first time
     void setBrokenState();

src/extensions/applicationextension/mextensionhandle_p.h
@@ -30,6 +30,10 @@
 
 class MAction;
 
+#ifdef HAVE_CONTEXTSUBSCRIBER
+class ContextProperty;
+#endif
+
 /*!
  * A private data class for MExtensionHandle.
  */
@@ -90,6 +94,11 @@ public:
 
     //! A list of remote actions
     QList<MAction *> remoteActions;
+
+#ifdef HAVE_CONTEXTSUBSCRIBER
+    //! Context property for getting the screen blanking status
+    ContextProperty *screenBlankProperty;
+#endif
 
     //! D-Bus service of the Package Manager
     static const QString PACKAGE_MANAGER_DBUS_SERVICE;

src/extensions/applicationextension/mextensionrunner.cpp
@@ -64,6 +64,9 @@
 #ifdef QT_OPENGL_LIB
 #include <QGLWidget>
 #endif
+#ifdef HAVE_CONTEXTSUBSCRIBER
+#include "contextproperty.h"
+#endif
 
 #include <typeinfo>
 
@@ -101,6 +104,10 @@ MExtensionRunner::MExtensionRunner() :
     widget(NULL),
     scene(NULL),
     view(NULL),
+#ifdef HAVE_CONTEXTSUBSCRIBER
+    screenBlankProperty(new ContextProperty("Session.State", this)),
+#endif
+    displayBlanked(false),
 #ifdef QT_OPENGL_LIB
     context(NULL),
 #endif
@@ -113,6 +120,9 @@ MExtensionRunner::MExtensionRunner() :
     connect(communicator, SIGNAL(messageReceived(MAppletMessage)), this, SLOT(messageReceived(MAppletMessage)));
     connect(aliveTimer, SIGNAL(timeout()), this, SLOT(hostProcessNotAlive()));
     connect(communicator, SIGNAL(connectionLost()), this, SLOT(hostProcessNotAlive()));
+#ifdef HAVE_CONTEXTSUBSCRIBER
+    connect(screenBlankProperty, SIGNAL(valueChanged()), this, SLOT(updateDisplayState()));
+#endif
 }
 
 MExtensionRunner::~MExtensionRunner()
@@ -170,6 +180,24 @@ bool MExtensionRunner::init(const QString &serverName)
     return true;
 }
 
+void MExtensionRunner::updateDisplayState()
+{
+#ifdef HAVE_CONTEXTSUBSCRIBER
+    bool blanked = screenBlankProperty->value().toString() == "blanked";
+
+    if(displayBlanked != blanked) {
+        displayBlanked = blanked;
+
+        if(blanked) {
+            aliveTimer->stop();
+        } else {
+            aliveTimer->start(ALIVE_TIMER_TIMEOUT);
+        }
+    }
+#endif
+}
+
+
 void MExtensionRunner::teardown()
 {
     delete scene;
@@ -193,7 +221,9 @@ void MExtensionRunner::messageReceived(const MAppletMessage &message)
     }
 
     aliveTimer->stop();
-    aliveTimer->start(ALIVE_TIMER_TIMEOUT);
+    if(!displayBlanked) {
+        aliveTimer->start(ALIVE_TIMER_TIMEOUT);
+    }
 
     switch (message.type()) {
     case MAppletMessage::MousePressMessage:

src/extensions/applicationextension/mextensionrunner.h
@@ -42,6 +42,9 @@ class QAction;
 #ifdef QT_OPENGL_LIB
 class QGLContext;
 #endif
+#ifdef HAVE_CONTEXTSUBSCRIBER
+class ContextProperty;
+#endif
 
 //! \internal
 /**
@@ -108,6 +111,11 @@ protected slots:
      */
     virtual void hostProcessNotAlive();
 
+    /*!
+     * \brief Stops or starts the alive timer depending on the display blanking state
+     */
+    void updateDisplayState();
+
 protected:
     //! AppletClient for communicating between the runner and the host processes.
     MAppletClient *communicator;
@@ -130,6 +138,14 @@ protected:
 
     //! List to hold object menu actions received from widget.
     QList<QAction *> widgetObjectMenuActions;
+
+#ifdef HAVE_CONTEXTSUBSCRIBER
+    //! Context property for getting the screen blanking status
+    ContextProperty *screenBlankProperty;
+#endif
+
+    //! True if the display is currently blanked
+    bool displayBlanked;
 
 #ifdef QT_OPENGL_LIB
     //! OpenGL context

tests/stubs/mextensionhandle_stub.h
@@ -63,6 +63,7 @@ class MExtensionHandleStub : public StubBase {
   virtual void remoteActionTriggered();
   virtual void setBrokenState();
   virtual void displayContextMenu(QList<QString> actions);
+  virtual void updateDisplayState();
 };
 
 // 2. IMPLEMENT STUB
@@ -93,6 +94,10 @@ void MExtensionHandleStub::kill() {
 
 void MExtensionHandleStub::reinit() {
   stubMethodEntered("reinit");
+}
+
+void MExtensionHandleStub::updateDisplayState() {
+  stubMethodEntered("updateDisplayState");
 }
 
 void MExtensionHandleStub::setAliveResponseTimeout(uint timeout) {
@@ -394,5 +399,8 @@ void MExtensionHandle::displayContextMenu(QList<QString> actions) {
   gMExtensionHandleStub->displayContextMenu(actions);
 }
 
+void MExtensionHandle::updateDisplayState() {
+  gMExtensionHandleStub->updateDisplayState();
+}
 
 #endif

tests/ut_mapplethandle/ut_mapplethandle.pro
@@ -6,6 +6,11 @@ INCLUDEPATH += \
     $$MSRCDIR/corelib/style
 QT += xml
 
+contains(DEFINES, HAVE_CONTEXTSUBSCRIBER) {
+    CONFIG += link_pkgconfig
+    PKGCONFIG += contextsubscriber-1.0
+}
+        
 MODEL_HEADERS += \
     $$MSRCDIR/corelib/widgets/mwidgetmodel.h \
     $$MSRCDIR/extensions/applicationextension/mextensionhandlemodel.h \

tests/ut_mextensionhandle/ut_mextensionhandle.cpp
@@ -55,6 +55,10 @@
 #include <mappletid_stub.h>
 #include <mscenemanager.h>
 
+#ifdef HAVE_CONTEXTSUBSCRIBER
+#include "contextproperty.h"
+#endif
+
 MApplication *app;
 
 bool visibility;
@@ -73,6 +77,15 @@ void MWidget::contextMenuEvent(QGraphicsSceneContextMenuEvent *)
 {
     Ut_MExtensionHandle::contextMenuOpened = true;
 }
+
+// ContextProperty stubs
+#ifdef HAVE_CONTEXTSUBSCRIBER
+QString gContextPropertyValue = "";
+QVariant ContextProperty::value() const
+{
+    return gContextPropertyValue;
+}
+#endif
 
 // MSceneManager stubs (used by MExtensionHandle)
 void MSceneManager::appearSceneWindow(MSceneWindow *, MSceneWindow::DeletionPolicy)
@@ -163,11 +176,26 @@ bool MAppletCommunicator::sendMessage(const MAppletMessage &message)
 }
 
 // QTimer stubs
-bool timerCalled = false;
+QList<QTimer*> gQTimerStartCalls;
+void QTimer::start()
+{
+    gQTimerStartCalls.append(this);
+    id = 1;
+}
+
+void QTimer::start(int)
+{
+    start();
+}
+
+bool gQTimerStopCalled = false;
+QList<QTimer*> gQTimerStopCalls;
 
 void QTimer::stop()
 {
-    timerCalled = true;
+    gQTimerStopCalled = true;
+    gQTimerStopCalls.append(this);
+    id = -1;
 }
 
 int elapsedQTime = 0;
@@ -234,7 +262,13 @@ void Ut_MExtensionHandle::init()
 
     listenForConnection = true;
     elapsedQTime = 0;
-    timerCalled = false;
+    gQTimerStartCalls.clear();
+    gQTimerStopCalled = false;
+    gQTimerStopCalls.clear();
+
+#ifdef HAVE_CONTEXTSUBSCRIBER
+    gContextPropertyValue = "";
+#endif
 
     gStartQProcess = true;
 
@@ -405,7 +439,7 @@ void Ut_MExtensionHandle::testAppletCommunication()
     handle->init("/bin/true", "metaDataFileName");
     MAppletAliveMessageResponse message;
     handle->messageReceivedFromRunner(message);
-    QCOMPARE(timerCalled, true);
+    QCOMPARE(gQTimerStopCalled, true);
 }
 
 void Ut_MExtensionHandle::testRelayingMousePress()
@@ -643,5 +677,28 @@ void Ut_MExtensionHandle::testSetSizeHints()
     handle->setSizeHints(sizeHints);
     QCOMPARE(handle->model()->sizeHints(), sizeHints);
 }
+
+#ifdef HAVE_CONTEXTSUBSCRIBER
+void Ut_MExtensionHandle::testWhenDisplayBlankedThenAliveCheckingStops()
+{
+    handle->init("/bin/true", "metaDataFileName");
+    emit connectionFromRunnerEstablished();
+    QVERIFY(qFind(gQTimerStartCalls, &handle->privateClass()->aliveTimer) != gQTimerStartCalls.end());
+    QVERIFY(qFind(gQTimerStartCalls, &handle->privateClass()->communicationTimer) != gQTimerStartCalls.end());
+
+    gContextPropertyValue = "blanked";
+    gQTimerStopCalls.clear();
+    handle->updateDisplayState();
+    QVERIFY(qFind(gQTimerStopCalls, &handle->privateClass()->aliveTimer) != gQTimerStopCalls.end());
+    QVERIFY(qFind(gQTimerStopCalls, &handle->privateClass()->communicationTimer) != gQTimerStopCalls.end());
+
+    gContextPropertyValue = "";
+    gQTimerStartCalls.clear();
+    handle->updateDisplayState();
+    QVERIFY(qFind(gQTimerStartCalls, &handle->privateClass()->aliveTimer) != gQTimerStartCalls.end());
+    QVERIFY(qFind(gQTimerStartCalls, &handle->privateClass()->communicationTimer) != gQTimerStartCalls.end());
+}
+#endif
+
 
 QTEST_APPLESS_MAIN(Ut_MExtensionHandle)

commit ff71b254bfa1c3b8a211d764576cb78dac16a52f

Fixes: Build break without context subscriber.
RevBy: TrustMe.

Details: Build break introduced in f891042021f2593d74c33de175f2c76077355649.

src/extensions/applicationextension/mextensionhandle.cpp
@@ -167,9 +167,9 @@ MExtensionHandle::~MExtensionHandle()
 
 void MExtensionHandle::updateDisplayState()
 {
+#ifdef HAVE_CONTEXTSUBSCRIBER
     Q_D(MExtensionHandle);
 
-#ifdef HAVE_CONTEXTSUBSCRIBER
     if(state() == MExtensionHandleModel::RUNNING) {
         bool blanked = d->screenBlankProperty->value().toString() == "blanked";
 