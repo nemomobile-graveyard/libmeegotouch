nemomobile-graveyard/libmeegotouch

commit 56ae58cece20edf526036567129e5b09433c47de

Fixes: NB#212448 - MWindow constructor causes ContextProperty::waitForSubscription() to be called, which may cause all kinds of side effects in an application
RevBy: Dominik
Details: Now waitForSubscription will not be called on context properties when constructing MWindow instance.
It will be called either when:
- show is called
- user will request keyboard state before subscription.

Few other minor corrections.

src/corelib/events/mkeyboardstatetracker.cpp
@@ -44,8 +44,6 @@ MKeyboardStateTrackerPrivate::MKeyboardStateTrackerPrivate(MKeyboardStateTracker
     QObject::connect(&keyboardOpenConf, SIGNAL(valueChanged()),
                      q, SIGNAL(stateChanged()));
 #endif
-
-    initContextSubscriber();
 }
 
 void MKeyboardStateTrackerPrivate::initContextSubscriber()
@@ -58,6 +56,16 @@ void MKeyboardStateTrackerPrivate::initContextSubscriber()
 #elif defined(M_OS_MAEMO5)
     present = true;
 #endif
+}
+
+void MKeyboardStateTrackerPrivate::subscribe()
+{
+    keyboardOpenProperty.subscribe();
+}
+
+void MKeyboardStateTrackerPrivate::unsubscribe()
+{
+    keyboardOpenProperty.unsubscribe();
 }
 
 MKeyboardStateTracker::MKeyboardStateTracker() :
@@ -91,9 +99,12 @@ bool MKeyboardStateTracker::isOpen() const
     bool val = false;
 #ifdef HAVE_CONTEXTSUBSCRIBER
     Q_D(const MKeyboardStateTracker);
-    if (isPresent()) {
-        val = d->keyboardOpenProperty.value().toBool();
+    MKeyboardStateTrackerPrivate* trackerPriv = const_cast<MKeyboardStateTrackerPrivate*>(d);
+    if (!isPresent()) {
+        trackerPriv->subscribe();
+        trackerPriv->initContextSubscriber();
     }
+    val = d->keyboardOpenProperty.value().toBool();
 #elif defined(M_OS_MAEMO5)
     Q_D(const MKeyboardStateTracker);
     val = d->keyboardOpenConf.value().toBool();

src/corelib/widgets/mwindow.cpp
@@ -110,7 +110,9 @@ MWindowPrivate::MWindowPrivate() :
     if (window)
         angle = window->orientationAngle();
     else
-        angle = MOrientationTracker::instance()->orientationAngle();
+        //only create instance of orientation tracker at the moment
+        MOrientationTracker::instance();
+
 
     timeSinceLastPaintInSwitcher.invalidate();
 }
@@ -401,11 +403,13 @@ void MWindowPrivate::applyStartupWindowBackground()
 void MWindowPrivate::resolveOrientationRules() {
     Q_Q(MWindow);
 
+#ifdef Q_WS_X11
     if ((!isIconicState || isAlwaysMapped) && !q->isOnDisplay()) {
         MOrientationTracker::instance()->d_ptr->startFollowingCurrentAppWindow(q, true);
     } else {
         MOrientationTracker::instance()->d_ptr->stopFollowingCurrentAppWindow(q, true);
     }
+#endif //Q_WS_X11
 
     MOrientationTracker::instance()->d_ptr->resolveIfOrientationUpdatesRequired();
 
@@ -603,7 +607,7 @@ void MWindowPrivate::ensureOrientationAngleIsUpToDateBeforeShowing()
 
         // We are about to be shown but our orientation angle is outdated.
 
-        MOrientationTracker::instance()->d_ptr->updateOrientationAngle();
+        MOrientationTracker::instance()->d_ptr->subscribeToSensorProperies();
 
         MOnDisplayChangeEvent ev(true, QRectF(QPointF(0, 0), q->visibleSceneSize()));
         MApplication::instance()->sendEvent(q, &ev);
@@ -1265,6 +1269,9 @@ void MWindow::paintEvent(QPaintEvent *event)
 bool MWindow::event(QEvent *event)
 {
     Q_D(MWindow);
+    if(event->type() == QEvent::Show) {
+        MOrientationTracker::instance()->d_func()->waitForSensorPropertiesToSubscribe();
+    }
 
     if ((event->type() == QEvent::Show && !isMinimized()) || event->type() == QEvent::WindowActivate) {
         MComponentData::setActiveWindow(this);

src/corelib/events/mkeyboardstatetracker.h
@@ -65,6 +65,8 @@ private:
 
     Q_DISABLE_COPY(MKeyboardStateTracker)
     Q_DECLARE_PRIVATE(MKeyboardStateTracker)
+
+    friend class MOrientationTrackerPrivate;
 };
 
 #endif // MKEYBOARDSTATETRACKER_H

src/corelib/events/mkeyboardstatetracker_p.h
@@ -33,7 +33,6 @@ class MKeyboardStateTrackerPrivate
 public:
     MKeyboardStateTrackerPrivate(MKeyboardStateTracker *controller);
 
-protected:
     static MKeyboardStateTracker *tracker;
 #ifdef HAVE_CONTEXTSUBSCRIBER
     ContextProperty keyboardOpenProperty;
@@ -42,8 +41,9 @@ protected:
 #endif
     MKeyboardStateTracker *q_ptr;
 
-private:
     void initContextSubscriber();
+    void unsubscribe();
+    void subscribe();
 
     bool present;
     Q_DECLARE_PUBLIC(MKeyboardStateTracker)

src/corelib/events/morientationtracker.cpp
@@ -34,6 +34,7 @@
 
 #include "morientationtracker.h"
 #include "morientationtracker_p.h"
+#include "mkeyboardstatetracker_p.h"
 
 MOrientationTracker *MOrientationTrackerPrivate::tracker = 0;
 
@@ -83,7 +84,6 @@ MOrientationTrackerPrivate::MOrientationTrackerPrivate(MOrientationTracker *cont
     connect(MKeyboardStateTracker::instance(), SIGNAL(stateChanged()),
             this, SLOT(updateOrientationAngle()));
 
-    waitForSensorPropertiesToSubscribe();
 #endif //HAVE_CONTEXTSUBSCRIBER
 
     isCoveredChanged();
@@ -166,8 +166,6 @@ void MOrientationTrackerPrivate::updateOrientationAngle()
 {
 #ifdef HAVE_CONTEXTSUBSCRIBER
     if (!isSubscribed) {
-        mWarning("MOrientationTrackerPrivate::updateOrientationAngle") << "orientation update requested"
-                << " when unsubscribed";
         return;
     }
 
@@ -315,6 +313,7 @@ void MOrientationTrackerPrivate::subscribeToSensorProperies()
         isCoveredProperty->subscribe();
         videoRouteProperty->subscribe();
         isFlatProperty->subscribe();
+        MKeyboardStateTracker::instance()->d_ptr->subscribe();
         waitForSensorPropertiesToSubscribe();
     }
     updateOrientationAngle();
@@ -328,6 +327,8 @@ void MOrientationTrackerPrivate::unsubscribeToSensorProperties()
         isCoveredProperty->unsubscribe();
         videoRouteProperty->unsubscribe();
         isFlatProperty->unsubscribe();
+        MKeyboardStateTracker::instance()->d_ptr->unsubscribe();
+        MKeyboardStateTracker::instance()->d_ptr->present = false;
     }
 }
 
@@ -337,6 +338,7 @@ void MOrientationTrackerPrivate::waitForSensorPropertiesToSubscribe()
     isCoveredProperty->waitForSubscription();
     videoRouteProperty->waitForSubscription();
     isFlatProperty->waitForSubscription();
+    MKeyboardStateTracker::instance()->d_ptr->initContextSubscriber();
     isSubscribed = true;
     hasJustSubscribed = true;
 }

tests/ut_morientationtracker/ut_morientationtracker.cpp
@@ -49,7 +49,9 @@ bool MDeviceProfile::orientationAngleIsSupported(M::OrientationAngle angle, bool
 void Ut_MOrientationTracker::init()
 {
     window1 = new MWindow;
+    window1->show();
     window2 = new MWindow;
+    window2->show();
 }
 
 void Ut_MOrientationTracker::cleanup()

commit 285172f4820db85bed325407723838e4f4328615

Changes: Fixes build break without context subscriber.
RevBy: TrustMe.

Details: Build break was introduced in 56ae58cece20edf526036567129e5b09433c47de.

src/corelib/events/mkeyboardstatetracker.cpp
@@ -60,12 +60,16 @@ void MKeyboardStateTrackerPrivate::initContextSubscriber()
 
 void MKeyboardStateTrackerPrivate::subscribe()
 {
+#ifdef HAVE_CONTEXTSUBSCRIBER
     keyboardOpenProperty.subscribe();
+#endif
 }
 
 void MKeyboardStateTrackerPrivate::unsubscribe()
 {
+#ifdef HAVE_CONTEXTSUBSCRIBER
     keyboardOpenProperty.unsubscribe();
+#endif
 }
 
 MKeyboardStateTracker::MKeyboardStateTracker() :

src/corelib/widgets/mwindow.cpp
@@ -605,9 +605,10 @@ void MWindowPrivate::ensureOrientationAngleIsUpToDateBeforeShowing()
     if (!q->isVisible() && !q->isOrientationAngleLocked()
         && MOrientationTracker::instance()->orientationAngle() != q->orientationAngle()) {
 
+#ifdef HAVE_CONTEXTSUBSCRIBER
         // We are about to be shown but our orientation angle is outdated.
-
         MOrientationTracker::instance()->d_ptr->subscribeToSensorProperies();
+#endif
 
         MOnDisplayChangeEvent ev(true, QRectF(QPointF(0, 0), q->visibleSceneSize()));
         MApplication::instance()->sendEvent(q, &ev);
@@ -1269,9 +1270,11 @@ void MWindow::paintEvent(QPaintEvent *event)
 bool MWindow::event(QEvent *event)
 {
     Q_D(MWindow);
+#ifdef HAVE_CONTEXTSUBSCRIBER
     if(event->type() == QEvent::Show) {
         MOrientationTracker::instance()->d_func()->waitForSensorPropertiesToSubscribe();
     }
+#endif
 
     if ((event->type() == QEvent::Show && !isMinimized()) || event->type() == QEvent::WindowActivate) {
         MComponentData::setActiveWindow(this);