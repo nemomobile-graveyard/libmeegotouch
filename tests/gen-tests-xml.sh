a0f3496128f249494e95d4d97cf744cf32143b27
Miroslav Safr <miroslav.safr@tieto.com>
2011-01-04 13:53:25 +0200

    Changes: Tests.xml generating script: commented out statistics printout RevBy: TrustMe Details: testrunner-lite takes tests.xml from stdout

--- tests/gen-tests-xml.sh
+++ tests/gen-tests-xml.sh
@@ -66,6 +66,6 @@ TESTSUITE_TEMPLATE="<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>
 </testdefinition>"
 
 echo "$TESTSUITE_TEMPLATE"
-echo "Skipped tests: $SKIPPED_TESTCASES"
-echo "UT:$UT_COUNT FT:$FT_COUNT SKIPPED=$SKIPPED_COUNT"
+#echo "Skipped tests: $SKIPPED_TESTCASES"
+#echo "UT:$UT_COUNT FT:$FT_COUNT SKIPPED=$SKIPPED_COUNT"
 
