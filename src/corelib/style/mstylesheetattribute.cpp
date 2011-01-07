b7d66fc5c97db7e657983148e44bed2387c675c3
Joaquim Rocha <jrocha@igalia.com>
2011-01-07 09:03:43 +0100

    Fixes: NB#217212 - libmeegotouch aborts on minor css problem...
    RevBy: Stanislav Ionascu, Adrián Yanes, Armin Berres
    Merge-Request: 1179
    
    Details:
    Removes eventual blank strings from the results when splitting the
    attribute values.

--- src/corelib/style/mstylesheetattribute.cpp
+++ src/corelib/style/mstylesheetattribute.cpp
@@ -677,6 +677,7 @@ bool MStyleSheetAttribute::writeAttribute(const QString &filename,
         } else {
             //no quotes, just split the parameters
             list = value.split(' ');
+            list.removeAll("");
         }
 
         //no parameters
