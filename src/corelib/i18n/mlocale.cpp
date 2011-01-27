nemomobile-graveyard/libmeegotouch

commit 8a1528da5a1e7661595eb1806fcff9a61891076f

Fixes: NB#217234 - [TASK] Calling MLocale::formatDateTime() generates 98 stat64() calls to files that do not ever exist
RevBy: Mike FABIAN

Adds a cache for MCalendar and
MLocale::formatDateTime(const MCalendar &mCalendar,const QString &formatString).

src/corelib/i18n/mlocale_p.h
@@ -157,6 +157,8 @@ public:
 
     bool isValidCountryCode( const QString& code ) const;
 
+    void dropCaches();
+
     bool _valid;
 
     // the default locale is used for messages and other categories if not
@@ -187,6 +189,7 @@ public:
     icu::NumberFormat *_numberFormatLcTime;
     mutable QCache<QString, icu::DateFormat> _dateFormatCache;
     mutable QCache<QString, icu::SimpleDateFormat> _simpleDateFormatCache;
+    mutable QCache<QString, QString> _icuFormatStringCache;
 #endif
 
     // translations for two supported translation categories
@@ -209,6 +212,9 @@ public:
     MGConfItem currentLcMonetaryItem;
     MGConfItem currentLcTelephoneItem;
 #endif
+
+    // calendar instance used formatDateTimeICU()
+    MCalendar *_pDateTimeCalendar;
 
     MLocale *q_ptr;
 };

commit 0c83beb3a1d0272db8dff898c6413d2e5d45985e

Fixes: Build break on Mac without ICU.
RevBy: TrustMe.

Details: Build break introduced in 8a1528da5a1e7661595eb1806fcff9a61891076f
Guard by ifdefs the missing class methods and members.

src/corelib/i18n/mlocale.cpp
@@ -628,7 +628,9 @@ MLocalePrivate::MLocalePrivate()
       currentLcMonetaryItem(SettingsLcMonetary),
       currentLcTelephoneItem(SettingsLcTelephone),
 #endif
+#ifdef HAVE_ICU
       _pDateTimeCalendar(0),
+#endif
       q_ptr(0)
 {
     if (translationPaths.isEmpty())
@@ -684,7 +686,9 @@ MLocalePrivate::MLocalePrivate(const MLocalePrivate &other)
       currentLcMonetaryItem(SettingsLcMonetary),
       currentLcTelephoneItem(SettingsLcTelephone),
 #endif
+#ifdef HAVE_ICU
       _pDateTimeCalendar(0),
+#endif
       q_ptr(0)
 {
 #ifdef HAVE_ICU
@@ -702,12 +706,12 @@ MLocalePrivate::~MLocalePrivate()
 #ifdef HAVE_ICU
     delete _numberFormat;
     delete _numberFormatLcTime;
-#endif
     // note: if tr translations are inserted into QCoreApplication
     // deleting the QTranslator removes them from the QCoreApplication
 
     delete _pDateTimeCalendar;
     _pDateTimeCalendar = 0;
+#endif
 }
 
 MLocalePrivate &MLocalePrivate::operator=(const MLocalePrivate &other)
@@ -752,6 +756,7 @@ MLocalePrivate &MLocalePrivate::operator=(const MLocalePrivate &other)
 
 void MLocalePrivate::dropCaches()
 {
+#ifdef HAVE_ICU
     // call this function when the MLocale has changed so that
     // cached data cannot be used any more
 
@@ -764,6 +769,7 @@ void MLocalePrivate::dropCaches()
 
     // drop cached formatString conversions
     _icuFormatStringCache.clear();
+#endif
 }
 
 bool MLocalePrivate::isValidCountryCode( const QString& code ) const

src/corelib/i18n/mlocale_p.h
@@ -214,7 +214,9 @@ public:
 #endif
 
     // calendar instance used formatDateTimeICU()
+#ifdef HAVE_ICU
     MCalendar *_pDateTimeCalendar;
+#endif
 
     MLocale *q_ptr;
 };