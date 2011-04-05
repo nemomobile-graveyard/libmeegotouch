654cef3803ff312190014e085f85bbfee792af19
Janne Heikkilä <janne.heikkila@symbio.com>
2011-04-05 13:47:34 +0300

    Fixes: NB#241826 - check why style == 0x0 issues in a couple of cores
    RevBy: Armin Berres
    MR: 1683

--- src/corelib/widgets/core/mwidgetcontroller.cpp
+++ src/corelib/widgets/core/mwidgetcontroller.cpp
@@ -285,6 +285,8 @@ void MWidgetControllerPrivate::createView()
         constructingView = true;
         view = MTheme::view(q);
         constructingView = false;
+    } else {
+        qCritical("Already creating a view for: %s. The view is not valid yet. Please make sure you are not accessing style during a construction of the view.", q->metaObject()->className());
     }
 
     if (view) {
@@ -627,10 +629,16 @@ bool MWidgetController::sceneEventFilter(QGraphicsItem *watched, QEvent *event)
 
 MWidgetStyleContainer &MWidgetController::style()
 {
+    if (!view())
+        qFatal("Cannot access style without view: %s", metaObject()->className());
+
     return const_cast<MWidgetStyleContainer &>(view()->style());
 }
 
 const MWidgetStyleContainer &MWidgetController::style() const
 {
+    if (!view())
+        qFatal("Cannot access style without view: %s", metaObject()->className());
+
     return view()->style();
 }
