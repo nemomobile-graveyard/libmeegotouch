nemomobile-graveyard/libmeegotouch

commit 1eaa20518966e27bca91acc231d75b9b04a7ed64

Fixes: NB#237669 - Possible memory leak in mthemedaemon process when sending IM messages
RevBy: Armin Berres

Details: The patch fixes two memory leaks that get triggered when sending IM messages.

src/corelib/theme/mthemedaemonclient.cpp
@@ -87,6 +87,7 @@ ImageResource *MThemeDaemonClient::findImageResource(const QString &imageId)
 void MThemeDaemonClient::reloadImagePaths(const QStringList &themes)
 {
     toBeDeletedThemeImageDirs.append(themeImageDirs);
+    qDeleteAll(themeImageDirs);
     themeImageDirs.clear();
 
     foreach(const QString & theme, themes) {

src/corelib/theme/mimagedirectory.cpp
@@ -154,10 +154,7 @@ void ImageResource::applyDebugColors(QImage *image)
 
 ImageResource::~ImageResource()
 {
-    if (cacheFile) {
-        cacheFile->unmap(buffer);
-        delete cacheFile;
-    }
+    deleteCacheFile();
 }
 
 void ImageResource::releasePixmap(const QSize &size)
@@ -256,6 +253,8 @@ QImage ImageResource::loadFromFsCache(const QSize& size, PixmapCacheEntry *cache
             stream >> imageFormat;
             meta.close();
 
+            deleteCacheFile();
+
             cacheFile = new QFile(cacheFileName);
             if (!cacheFile->open(QFile::ReadOnly)) {
                 delete cacheFile;
@@ -263,6 +262,7 @@ QImage ImageResource::loadFromFsCache(const QSize& size, PixmapCacheEntry *cache
                 return QImage();
             }
 
+            Q_ASSERT(!buffer);
             buffer = cacheFile->map(0, cacheFile->size());
             QImage image((const uchar*)buffer, imageSize.width(), imageSize.height(), (QImage::Format)imageFormat);
 
@@ -385,6 +385,16 @@ void ImageResource::fillCacheEntry(PixmapCacheEntry *cacheEntry, QImage &image, 
 #else
     MGraphicsSystemHelper::pixmapFromImage(cacheEntry, image, uniqueKey(), size);
 #endif
+}
+
+void ImageResource::deleteCacheFile()
+{
+    if (cacheFile) {
+        cacheFile->unmap(buffer);
+        buffer = 0;
+        delete cacheFile;
+        cacheFile = 0;
+    }
 }
 
 bool ImageResource::shouldBeCached()

src/corelib/theme/mimagedirectory.h
@@ -119,6 +119,8 @@ private:
 
     void fillCacheEntry(PixmapCacheEntry *cacheEntry, QImage &image, const QSize &size);
 
+    void deleteCacheFile();
+
 #ifdef  Q_WS_X11
     bool convertToX11;
 #endif

commit 93e4207e82008e48a653dafa7fd1189dfdffeb17

Changes: Fix crash in MThemeDaemonClient
RevBy: Armin Berres

Details: A regression has been introduced by commit 1eaa20518966e27bca91acc231d75b9b04a7ed64: MThemeImagesDirectories might get deleted twice.

src/corelib/theme/mthemedaemonclient.cpp
@@ -87,7 +87,6 @@ ImageResource *MThemeDaemonClient::findImageResource(const QString &imageId)
 void MThemeDaemonClient::reloadImagePaths(const QStringList &themes)
 {
     toBeDeletedThemeImageDirs.append(themeImageDirs);
-    qDeleteAll(themeImageDirs);
     themeImageDirs.clear();
 
     foreach(const QString & theme, themes) {