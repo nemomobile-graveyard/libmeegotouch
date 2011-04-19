nemomobile-graveyard/libmeegotouch

commit 49b47a13670c102c9cb3dfb616bb42c125d3447a

Changes: MLabel - make the minimum size equal to the preferred size if we have no word wrap or eliding enabled
RevBy: Peter Penz
Details: Fixes NB#236032 Subject label NOT shown completely in Header
info view if length exceeds   among others.

The idea is that if we have two labels side by side, one with eliding
and one without, we want the one without eliding to have all the space,
and the one with eliding to be the one that shrinks.

src/views/mlabelview_rich.cpp
@@ -173,14 +173,19 @@ QSizeF MLabelViewRich::sizeHint(Qt::SizeHint which, const QSizeF &constraint) co
 {
     switch (which) {
     case Qt::MinimumSize: {
-        QFontMetrics fm(viewPrivate->controller->font());
-        //Resize text document to maximum width to find the height of one line
-        //This follows the QLabel implementation
-        qreal oldWidth = textDocument.textWidth();
-        textDocument.setTextWidth(QWIDGETSIZE_MAX);
-        qreal height = textDocument.size().height();
-        textDocument.setTextWidth(oldWidth);
-        return QSizeF(fm.width('x'), height);
+        //If we have word wrap or eliding, the minimum size is the size of a single letter,
+        if (wrap() || viewPrivate->model()->textElide()) {
+            QFontMetrics fm(viewPrivate->controller->font());
+            //Resize text document to maximum width to find the height of one line
+            //This follows the QLabel implementation
+            qreal oldWidth = textDocument.textWidth();
+            textDocument.setTextWidth(QWIDGETSIZE_MAX);
+            qreal height = textDocument.size().height();
+            textDocument.setTextWidth(oldWidth);
+            return QSizeF(fm.width('x'), height);
+        }
+        //If word wrap and eliding are both disabled (the default) then fall through to preferred
+        //size case, so that the preferred size == minimum size.
     }
 
     case Qt::PreferredSize: {
@@ -267,6 +272,8 @@ bool MLabelViewRich::updateData(const QList<const char *>& modifications)
         } else if (member == MLabelModel::Color) {
             needUpdate = true;
             textDocumentDirty = true;
+        } else if (member == MLabelModel::TextElide) {
+            needUpdate = true;
         } else if(member == MLabelModel::WrapMode) {
             if (model->wordWrap()) {
                 if (model->wrapMode() == QTextOption::NoWrap) {

src/views/mlabelview_simple.cpp
@@ -119,8 +119,13 @@ QSizeF MLabelViewSimple::sizeHint(Qt::SizeHint which, const QSizeF &constraint) 
 {
     switch (which) {
     case Qt::MinimumSize: {
-        QFontMetricsF fm(viewPrivate->controller->font());
-        return QSizeF(fm.width(QLatin1Char('x')), fm.height());
+        //If we have word wrap or eliding, the minimum size is the size of a single letter,
+        if (wrap() || viewPrivate->model()->textElide()) {
+            QFontMetricsF fm(viewPrivate->controller->font());
+            return QSizeF(fm.width(QLatin1Char('x')), fm.height());
+        }
+        //If word wrap and eliding are both disabled (the default) then fall through to preferred
+        //size case, so that the preferred size == minimum size.
     }
     case Qt::PreferredSize: {
         qreal width = constraint.width();
@@ -171,6 +176,8 @@ bool MLabelViewSimple::updateData(const QList<const char *>& modifications)
             if (model->wordWrap()) {
                 viewPrivate->textOptions.setWrapMode(model->wrapMode());
             }
+            needUpdate = true;
+        } else if (member == MLabelModel::TextElide) {
             needUpdate = true;
         } else if (member == MLabelModel::WordWrap) {
             if (model->wordWrap()) {

tests/ut_mlabel/ut_mlabel.cpp
@@ -39,6 +39,13 @@
 #include <QTextDocument>
 #include <QTextLayout>
 
+#include <QLabel>
+
+#define SHOW_IMAGE(image) \
+    do { \
+        QLabel label; label.setPixmap(QPixmap::fromImage(elided)); label.show(); QTest::qWait(1000); \
+    } while(0)
+
 #define SAVE_IMAGE(fileName, image) \
     do{ \
         QFile f(fileName); f.open(QIODevice::WriteOnly); image.save(&f, "PNG"); f.close(); \
@@ -262,6 +269,7 @@ void Ut_MLabel::testTextWordWrap()
     QVERIFY(text == label->text());
 
     label->setWordWrap(true);
+    label->resize(400,400);
     QVERIFY(label->wordWrap() == true);
     QImage wrapped = captureImage(label);
 
@@ -293,19 +301,17 @@ void Ut_MLabel::testTextElide()
 
     label->setWordWrap(false);
     label->setTextElide(true);
+    label->resize(400,400);
     QVERIFY(label->textElide() == true);
     QImage elided = captureImage(label);
 
     label->setTextElide(false);
+    label->resize(400,400);
     QVERIFY(label->textElide() == false);
     QImage unelided = captureImage(label);
 
     label->setLayoutDirection(Qt::RightToLeft);
     QImage elidedRtl = captureImage(label);
-
-    //elided.save("elided", "PNG");
-    //unelided.save("unelided", "PNG");
-    //elidedRtl.save("elidedrtl", "PNG");
 
     QVERIFY(elided != unelided);
     QVERIFY(elided != elidedRtl);
@@ -326,18 +332,21 @@ void Ut_MLabel::testRichTextElide()
     QVERIFY(text == label->text());
 
     label->setTextElide(true);
+    label->resize(400,400);
     QVERIFY(label->textElide() == true);
     QImage elided = captureImage(label);
 
     label->setTextElide(false);
+    label->resize(400,400);
     QVERIFY(label->textElide() == false);
     QImage unelided = captureImage(label);
 
-    QImage leftUnelided = unelided.copy(0, 0, unelided.width() / 2, unelided.height());
-    QImage rightUnelided = unelided.copy(unelided.width() / 2, 0, unelided.width() / 2, unelided.height());
+    int cropWidth = 200;
+    QImage leftUnelided = unelided.copy(0, 0, cropWidth, unelided.height());
+    QImage rightUnelided = unelided.copy(cropWidth, 0, unelided.width() - cropWidth, unelided.height());
 
-    QImage leftElided = elided.copy(0, 0, elided.width() / 2, elided.height());
-    QImage rightElided = elided.copy(elided.width() / 2, 0, elided.width() / 2, elided.height());
+    QImage leftElided = elided.copy(0, 0, cropWidth, elided.height());
+    QImage rightElided = elided.copy(cropWidth, 0, elided.width() - cropWidth, elided.height());
 
     QCOMPARE(elided.isNull(), false);
     QCOMPARE(unelided.isNull(), false);
@@ -552,11 +561,15 @@ void Ut_MLabel::testSizeHint()
     QFontMetricsF fm(label->font());
     if (constraint.width() > 0.0) {
         QCOMPARE(minSizeHint.width(), constraint.width());
+    } else if (wrapMode == QTextOption::NoWrap || wrapMode == QTextOption::ManualWrap ) {
+        QCOMPARE(minSizeHint.width(), prefSizeHint.width());
     } else {
         QCOMPARE(minSizeHint.width(), fm.width(QLatin1Char('x')));
     }
     if (constraint.height() > 0.0) {
         QCOMPARE(minSizeHint.width(), constraint.height());
+    } else if (wrapMode == QTextOption::NoWrap || wrapMode == QTextOption::ManualWrap ) {
+        QCOMPARE(minSizeHint.height(), prefSizeHint.height());
     } else {
         QCOMPARE(minSizeHint.height(), fm.height());
     }
@@ -604,6 +617,8 @@ void Ut_MLabel::testSizeHint()
         QSizeF prefSizeHint = label->preferredSize();
         QSizeF maxSizeHint = label->maximumSize();
 
+        if (wrapMode == QTextOption::NoWrap || wrapMode == QTextOption::ManualWrap )
+            width = qMax(width, minSizeHint.width());
         QCOMPARE(prefSizeHint.width(), width);
 
         label->resize(prefSizeHint);
@@ -855,6 +870,7 @@ void Ut_MLabel::wrapModes()
 {
     QFETCH(QString, text);
     label->setText(text);
+    label->setMaximumSize(100,800);
     label->resize(100, 800);
 
     QImage defaultMode = captureImage(label);

commit 4b744a79d13b8218eaed2a5cbef5b1e77ce34727

Fixes: NB#248110 - disabling elide mode for MLabel does not change appearance
RevBy: Stanislav Ionascu
MergeRequest: 1752

Details: A regression has been introduced by commit 49b47a13670c102c9cb3dfb616bb42c125d3447a: The additional check for MLabelModel::TextElide resulted in skipping the second check, where textDocumentDirty has been set to true.

src/views/mlabelview_rich.cpp
@@ -272,8 +272,6 @@ bool MLabelViewRich::updateData(const QList<const char *>& modifications)
         } else if (member == MLabelModel::Color) {
             needUpdate = true;
             textDocumentDirty = true;
-        } else if (member == MLabelModel::TextElide) {
-            needUpdate = true;
         } else if(member == MLabelModel::WrapMode) {
             if (model->wordWrap()) {
                 if (model->wrapMode() == QTextOption::NoWrap) {
@@ -301,6 +299,7 @@ bool MLabelViewRich::updateData(const QList<const char *>& modifications)
             viewPrivate->textOptions.setAlignment(model->alignment());
             textDocumentDirty = true;
         } else if (member == MLabelModel::TextElide) {
+            needUpdate = true;
             textDocumentDirty = true;
         } else if (member == MLabelModel::UseModelFont || member == MLabelModel::Font) {
             textDocument.setTextWidth(viewPrivate->boundingRect().width());