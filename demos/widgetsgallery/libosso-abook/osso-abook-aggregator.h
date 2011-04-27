b2591a2ba066f8ae8579f44583fab2b1a3a80a35
Eduardo Lima Mitev <elima@igalia.com>
2011-04-27 19:28:24 +0200

    Fixes: NB#247795 - Legal bug in libmeegotouch
    RevBy: TrustMe
    
    Details: Fixes comment block to have proper copyright and license in all files
    specified and removes the copyrighted image.

--- demos/widgetsgallery/libosso-abook/osso-abook-aggregator.h
+++ demos/widgetsgallery/libosso-abook/osso-abook-aggregator.h
@@ -1,10 +1,21 @@
-/*
- * This file is part of libosso-abook
- *
- * Copyright (C) 2008-2009 Nokia Corporation. All rights reserved.
- *
- * Contact: Joergen Scheibengruber <jorgen.scheibengruber@nokia.com>
- */
+/***************************************************************************
+**
+** Copyright (C) 2008-2009 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (directui@nokia.com)
+**
+** This file is part of libmeegotouch.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at directui@nokia.com.
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation
+** and appearing in the file LICENSE.LGPL included in the packaging
+** of this file.
+**
+****************************************************************************/
 
 #ifndef __OSSO_ABOOK_AGGREGATOR_H__
 #define __OSSO_ABOOK_AGGREGATOR_H__
