nemomobile-graveyard/libmeegotouch

commit ad2eca1eaccd4ac3b6b7b46bdc0dea1dffc924dd

Changes: Optimize emission of MOrientationChangeEvent
RevBy: Marcin Miklas
Details:
Currently when the orientation changes MScenenManager sends a
MOrientationChangeEvent to every single QGraphicsWidget in the scene.

In a regular drill-down scenario that will include all the previous pages that
you can reach by pressing the back button.

The optimization idea is to send MOrientationChangeEvent events only to
widgets in scene windows whose state is different than
MSceneWindow::Disappeared. And when a scene window is about to leave its
Disappeared state MSceneManager checks if it has an outdated orientation and if
so sends MOrientationChangeEvent events for its widgets to get them up to date.

When a widget gets an MOrientationChangeEvent it updates and re-apply its
style.

This optimization will reduce the total time it takes for an orientation change
to happen. More specifically, it will reduce its setup time, i.e. the time it
takes between MOrientationTracker tells the new orientation and the orientation
animation starting to run.

Fixes: NB#249745 -[TASK] Optimize emission of MOrientationChangeEvent

src/corelib/scene/mscenemanager.cpp
@@ -378,7 +378,7 @@ void MSceneManagerPrivate::_q_changeGlobalOrientationAngle()
     if (oldOrientation != newOrientation) {
         emit q->orientationAboutToChange(q->orientation());
 
-        notifyWidgetsAboutOrientationChange();
+        notifyWidgetsBeingRenderedAboutOrientationChange();
         setSceneWindowGeometries();
 
         // emit signal after sending the orientation event to widgets (in case someone
@@ -676,9 +676,6 @@ void MSceneManagerPrivate::addSceneWindow(MSceneWindow *sceneWindow)
     if (!sceneWindow->d_func()->sceneManager) {
         // Just add it
         addUnmanagedSceneWindow(sceneWindow);
-        // This window was not receiving any events. Might not know about orientation change yet.
-        MOrientationChangeEvent event(MDeviceProfile::instance()->orientationFromAngle(angle));
-        scene->sendEvent(sceneWindow, &event);
     } else {
         if (sceneWindow->d_func()->sceneManager != q) {
             // scene window belongs to another scene manager, remove it from him.
@@ -1021,9 +1018,43 @@ void MSceneManagerPrivate::updateStatusBarGeometryProperty()
 }
 #endif
 
-void MSceneManagerPrivate::notifyWidgetsAboutOrientationChange()
+void MSceneManagerPrivate::notifyWidgetsBeingRenderedAboutOrientationChange()
 {
-    MScenePrivate::notifySceneAboutOrientationChange(scene, MDeviceProfile::instance()->orientationFromAngle(angle));
+    Q_Q(MSceneManager);
+    if (!scene)
+        return;
+
+    MOrientationChangeEvent event(q->orientation());
+
+    Q_FOREACH(MSceneWindow *sceneWindow, sceneWindowStack.list()) {
+        sendEventRecursively(sceneWindow, &event);
+        if (sceneWindow->d_func()->effect)
+            sendEventRecursively(sceneWindow->d_func()->effect, &event);
+    }
+}
+
+void MSceneManagerPrivate::sendEventRecursively(QGraphicsItem *item, QEvent *event)
+{
+    // event handlers might remove items from the scene
+    // so we must check if item it's still there
+    if (!scene->items().contains(item)) {
+        return;
+    }
+
+    scene->sendEvent(item, event);
+    // Sending an event to a widget means only its direct layout gets the orientation
+    // change event.
+    // To ensure that the event it sent to all layouts, we have to manually send it to
+    // the sub-layouts (layout inside a layout)
+    if (item->isWidget()) {
+        QGraphicsLayout *layout = static_cast<QGraphicsWidget *>(item)->layout();
+        if (layout)
+            MScenePrivate::sendEventToAllSubLayouts(layout, event);
+    }
+
+    Q_FOREACH(QGraphicsItem *child, item->children()) {
+        sendEventRecursively(child, event);
+    }
 }
 
 QRectF MSceneManagerPrivate::calculateAvailableSceneRect(MSceneWindow *sceneWindow) const
@@ -1299,6 +1330,18 @@ void MSceneManagerPrivate::prepareWindowShow(MSceneWindow *window)
     window->d_func()->dismissed = false;
 
     sceneWindowStack.add(window);
+
+    // This scene window was not receiving any orientation change events.
+    // Therefore it might have an outdated orientation.
+    M::Orientation currentOrientation = q->orientation();
+    if (window->d_func()->orientationFromLastEvent != currentOrientation) {
+        // Update the orientaton of that scene window
+        MOrientationChangeEvent event(currentOrientation);
+        sendEventRecursively(window, &event);
+        if (window->d_func()->effect)
+            sendEventRecursively(window->d_func()->effect, &event);
+        Q_ASSERT(window->d_func()->orientationFromLastEvent == currentOrientation);
+    }
 
     setSceneWindowGeometry(window);
     setupLayerEffectForSceneWindow(window);

src/corelib/scene/mscenemanager_p.h
@@ -127,7 +127,8 @@ public:
     void setSceneWindowGeometries();
     QGraphicsWidget *rootElementForSceneWindow(MSceneWindow *sceneWindow) const;
     void updateSceneWindowRootElement(MSceneWindow *sceneWindow);
-    void notifyWidgetsAboutOrientationChange();
+    void notifyWidgetsBeingRenderedAboutOrientationChange();
+    void sendEventRecursively(QGraphicsItem *item, QEvent *event);
     void rotateToAngle(M::OrientationAngle newAngle);
     void setOrientationAngleWithoutAnimation(M::OrientationAngle newAngle);
 

src/corelib/widgets/mscenewindow.cpp
@@ -36,6 +36,7 @@
 #include "mwindow.h"
 #include "mdialog.h"
 #include "mcancelevent.h"
+#include <morientationchangeevent.h>
 
 #include "mmetatypes.h"
 
@@ -355,6 +356,11 @@ bool MSceneWindow::event(QEvent *event)
             d->focusItemBeforeWindowBlocked->setFocus(Qt::ActiveWindowFocusReason);
         }
         d->focusItemBeforeWindowBlocked = 0;
+    } else if (event->type() == MOrientationChangeEvent::eventNumber()) {
+        MOrientationChangeEvent *orientationChangeEvent =
+                static_cast<MOrientationChangeEvent *>(event);
+
+        d->orientationFromLastEvent = orientationChangeEvent->orientation();
     }
 
     return MWidgetController::event(event);

src/corelib/widgets/mscenewindow_p.h
@@ -74,6 +74,9 @@ public:
 
     // Focused item before window blocked.
     QPointer<QGraphicsWidget> focusItemBeforeWindowBlocked;
+
+    // The orientation received from the last MOrientationChangeEvent
+    M::Orientation orientationFromLastEvent;
 };
 
 class MSceneWindowTestInterface : public QObject

commit 3ed8fea609c7e31f6cdf947e5514385bcdd00497

Changes: New tests for ut_mtoolbarview
RevBy: Marcin Miklas
Details: Those new tests cover the use case broken by commit
ad2eca1eaccd4ac3b6b7b46bdc0dea1dffc924dd ("Changes: Optimize emission of
MOrientationChangeEvent"). More specifically the one testing "isEmpty"
property.

tests/ut_mtoolbarview/ut_mtoolbarview.cpp
@@ -33,6 +33,7 @@
 #include <MLayout>
 #include <QGraphicsLinearLayout>
 #include <QPointer>
+#include <morientationchangeevent.h>
 
 #include "ut_mtoolbarview.h"
 #include "mtoolbarview_p.h"
@@ -54,7 +55,6 @@ void Ut_MToolBarView::initTestCase()
     appWin = new MApplicationWindow;
 
     m_scenewindow = new MSceneWindow;
-    appWin->sceneManager()->appearSceneWindowNow(m_scenewindow);
 }
 
 void Ut_MToolBarView::cleanupTestCase()
@@ -69,6 +69,7 @@ void Ut_MToolBarView::cleanupTestCase()
 void Ut_MToolBarView::init()
 {
     appWin->setOrientationAngle(M::Angle0);
+    appWin->sceneManager()->appearSceneWindowNow(m_scenewindow);
     m_toolbar = new MToolBar();
     QVERIFY(m_toolbar != 0);
     // need to be in a scene window to get orientation change events
@@ -1135,6 +1136,37 @@ void Ut_MToolBarView::testLabelOnlyAreNotCommonButtonsInTabBar()
 
     QVERIFY(buttonIconAndLabel->viewType() == "toolbartab");
     QVERIFY(buttonLabelOnly->viewType() == "toolbartab");
+}
+
+void Ut_MToolBarView::testGetOrientationChangeEventsWhenInDisappearedSceneWindow()
+{
+    EventSpy toolbarEventSpy;
+
+    appWin->setLandscapeOrientation();
+    appWin->sceneManager()->disappearSceneWindowNow(m_scenewindow);
+
+    m_toolbar->installEventFilter(&toolbarEventSpy);
+
+    appWin->setPortraitOrientation();
+
+    QVERIFY(toolbarEventSpy.eventTypesReceived.contains(MOrientationChangeEvent::eventNumber()));
+}
+
+void Ut_MToolBarView::testIsEmptyPropertyIsUpdatedWhenInDisappearedSceneWindow()
+{
+    MAction *actionIconAndLabel = new MAction("icon-l-search", "action", m_toolbar);
+    actionIconAndLabel->setLocation(MAction::ToolBarPortraitLocation);
+    m_toolbar->addAction(actionIconAndLabel);
+
+    appWin->setLandscapeOrientation();
+
+    QCOMPARE(m_toolbar->property("isEmpty").toBool(), true);
+
+    appWin->sceneManager()->disappearSceneWindowNow(m_scenewindow);
+
+    appWin->setPortraitOrientation();
+
+    QCOMPARE(m_toolbar->property("isEmpty").toBool(), false);
 }
 
 QTEST_APPLESS_MAIN(Ut_MToolBarView)

tests/ut_mtoolbarview/ut_mtoolbarview.h
@@ -74,6 +74,8 @@ private slots:
     void testLabelOnlyButtonsHaveDifferentStylename();
     void testLabelOnlyAreCommonButtonsInToolBar();
     void testLabelOnlyAreNotCommonButtonsInTabBar();
+    void testGetOrientationChangeEventsWhenInDisappearedSceneWindow();
+    void testIsEmptyPropertyIsUpdatedWhenInDisappearedSceneWindow();
 
 private:
     MToolBar *m_toolbar;
@@ -85,6 +87,22 @@ private:
     void createDummyActions(int count);
     MAction *createAction();
     bool isVisible(MButton *button);
+};
+
+class EventSpy : public QObject
+{
+    Q_OBJECT
+public:
+    EventSpy() {}
+    virtual ~EventSpy() {}
+
+    virtual bool eventFilter (QObject *watched, QEvent *event) {
+        Q_UNUSED(watched)
+        eventTypesReceived << event->type();
+        return false;
+    }
+
+    QList<QEvent::Type> eventTypesReceived;
 };
 
 #endif