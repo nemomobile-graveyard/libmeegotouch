b3186ea9ad75d0bf92cef78c5fd015bd27428aeb
Pekka Vuorela <pekka.ta.vuorela@nokia.com>
2011-06-15 13:15:22 +0300

    Fixes: NB#263763 - Setting font size in MRichTextEdit works unreliably
    RevBy: Stanislav Ionascu
    Details: Fix rounding in MDeviceProfile pixel<->point conversions

--- src/corelib/workspace/mdeviceprofile.cpp
+++ src/corelib/workspace/mdeviceprofile.cpp
@@ -302,7 +302,7 @@ qreal MDeviceProfile::mmToPixelsF(qreal mm)
 int MDeviceProfile::ptToPixels(qreal pt)
 {
     Q_D(const MDeviceProfile);
-    return pt * d->pixelsPerPtF;
+    return qRound(pt * d->pixelsPerPtF);
 }
 
 qreal MDeviceProfile::ptToPixelsF(qreal pt)
@@ -314,7 +314,7 @@ qreal MDeviceProfile::ptToPixelsF(qreal pt)
 int MDeviceProfile::pixelsToPt(int pixels)
 {
     Q_D(const MDeviceProfile);
-    return pixels / d->pixelsPerPtF;
+    return qRound(pixels / d->pixelsPerPtF);
 }
 
 qreal MDeviceProfile::pixelsToPtF(qreal pixels)
