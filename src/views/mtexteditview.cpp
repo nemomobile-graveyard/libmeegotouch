1dc37e5c38e87bc66495985d9fecfc127ff4c6b6
Stanislav Ionascu <stanislav.ionascu@nokia.com>
2011-06-30 10:04:52 +0300

    Changes: TextEdit should not set size policy in the constructor of the controller.
    RevBy: Daniel d'Andrada
    MergeRequest: 1979
    
    Details: Setting a size policy in the constructor of the widget controller will trigger view creation and recursive style reloading, without the posibility of overriding the view class.

--- src/views/mtexteditview.cpp
+++ src/views/mtexteditview.cpp
@@ -1819,6 +1819,15 @@ void MTextEditView::setupModel()
     delete d->maskedTextDocument;
     d->maskedTextDocument = 0;
 
+    if (model()->line() == MTextEditModel::SingleLine) {
+        d->controller->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+    } else {
+        //Set to expand vertically only in multiline mode
+        QSizePolicy policy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+        policy.setHeightForWidth(true);
+        d->controller->setSizePolicy(policy);
+    }
+
     if (model()->echo() != MTextEditModel::Normal) {
         d->initMaskedDocument();
     }
