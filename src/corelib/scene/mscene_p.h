f9a74c73123a23908724410db82bde2cf5fea77d
Adrian Yanes <ext-adrian.yanes@nokia.com>
2011-08-15 13:18:24 +0300

    Revert "Fixes: NB#265473, Text magnifier is not working in Nokia account creation"
    This reverts commit d2cf9086c08caca87e0157d8346aa768a7a909d7.

--- src/corelib/scene/mscene_p.h
+++ src/corelib/scene/mscene_p.h
@@ -93,7 +93,6 @@ public:
     static void sendEventToAllSubLayouts(QGraphicsLayout *layout, QEvent *event);
     static void notifySceneAboutOrientationChange(QGraphicsScene *scene, M::Orientation orientation);
 
-    static bool mouseSetsFocusForItem(QGraphicsItem *item);
     static QGraphicsItem *actualClickFocusTarget(QGraphicsItem *item);
 
 protected:
