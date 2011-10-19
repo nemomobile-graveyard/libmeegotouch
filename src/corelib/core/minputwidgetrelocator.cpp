nemomobile-graveyard/libmeegotouch

commit 7a34dbd204a141548cfcf6b6255105db29ca006a

Fixes: NB#268598, Text edit not fully visible in landscape sheet
RevBy: Daniel d'Andrada
Details: Exposed content rectangle was incorrectly calculated because it relied
on the object name of pannable viewport occupying the central slot in sheets.
This assumption is wrong if custom pannable viewport is set with MSheet::setCentralWidget.

https://meego.gitorious.org/meegotouch/libmeegotouch/merge_requests/2077

src/corelib/core/minputwidgetrelocator.cpp
@@ -217,13 +217,21 @@ const QRect &MInputWidgetRelocator::exposedContentRect()
     } else {
         // check if focused widget is in MSheet
         QGraphicsWidget *inputWidget = focusedWidget();
-        QGraphicsWidget *sheetPannableViewport = inputWidget ? inputWidget->parentWidget() : 0;
-        while (sheetPannableViewport && sheetPannableViewport->objectName() != "MSheetCentralSlotPannableViewport")
-            sheetPannableViewport = sheetPannableViewport->parentWidget();
+        QGraphicsWidget *parentWidget = inputWidget ? inputWidget->parentWidget() : 0;
+        QGraphicsWidget *sheetCentralViewport = 0;
 
-        if (sheetPannableViewport)
-            cachedExposedRect &= rootElement->mapRectFromItem(sheetPannableViewport,
-                                                              sheetPannableViewport->rect()).toRect();
+        // Take last pannable viewport found and assume it is occupying central slot.
+        while (parentWidget) {
+            if (parentWidget->inherits("MPannableViewport")) {
+                sheetCentralViewport = parentWidget;
+            }
+            parentWidget = parentWidget->parentWidget();
+        }
+
+        if (sheetCentralViewport) {
+            cachedExposedRect &= rootElement->mapRectFromItem(sheetCentralViewport,
+                                                              sheetCentralViewport->rect()).toRect();
+        }
     }
 
     // Map input panel rectangle to rootElement's orientation.

commit d76c98598b0c66a845524f187c1d15391417bba5

Fixes: NB#275691, Text edit in dialog moving upwards on every character.
RevBy: Michael Hasselmann
Details: Jumping caused by commit 7a34dbd204a141548cfcf6b6255105db29ca006a
which didn't take into account that MDialog's have MPannableViewport in them.

The case of single text edit in a dialog, in portrait, doesn't usuallly
induce relocation at all because text edit is already visible, with margins.
It used to, however, but commit 7f4b73eb67529e34a1454aa985a646d1fa11e73e has
changed something in MDialog's geometry and it therefore currently does not.

src/corelib/core/minputwidgetrelocator.cpp
@@ -219,16 +219,25 @@ const QRect &MInputWidgetRelocator::exposedContentRect()
         QGraphicsWidget *inputWidget = focusedWidget();
         QGraphicsWidget *parentWidget = inputWidget ? inputWidget->parentWidget() : 0;
         QGraphicsWidget *sheetCentralViewport = 0;
+        MSceneWindow *sceneWindow = 0;
 
         // Take last pannable viewport found and assume it is occupying central slot.
         while (parentWidget) {
             if (parentWidget->inherits("MPannableViewport")) {
                 sheetCentralViewport = parentWidget;
             }
+
+            // If inputWidget is inside sheet we must have a scene window.
+            if (!sceneWindow && qobject_cast<MSceneWindow *>(parentWidget)) {
+                sceneWindow = static_cast<MSceneWindow *>(parentWidget);
+            }
+
             parentWidget = parentWidget->parentWidget();
         }
 
-        if (sheetCentralViewport) {
+        if (sheetCentralViewport
+            && sceneWindow
+            && sceneWindow->windowType() == MSceneWindow::Sheet) {
             cachedExposedRect &= rootElement->mapRectFromItem(sheetCentralViewport,
                                                               sheetCentralViewport->rect()).toRect();
         }