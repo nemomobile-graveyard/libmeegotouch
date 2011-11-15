fa501b4a1dc43462fd461dff211a9dddef1fbc03
Maksim Kirillov <ext-maksim.kirillov@nokia.com>
2011-11-15 15:16:36 +0200

    Fixes: NB#291040, Text selection handles not in correct location after bolding/unbolding
    RevBy: Michael Hasselmann, Jarno Malmari
    Details:
     * resend document::contentsChanged() to overlay
       via MTextEditView::selectionChanged()
     * removed resending of controller::textChanged() as it is
       always doubled by the previous one

--- src/views/mtexteditview.cpp
+++ src/views/mtexteditview.cpp
@@ -857,9 +857,6 @@ void MTextEditViewPrivate::showSelectionOverlay()
                              Qt::UniqueConnection);
         }
 
-        QObject::connect(controller, SIGNAL(textChanged()),
-                         this, SLOT(mapSelectionChange()),
-                         Qt::UniqueConnection);
         QObject::connect(controller, SIGNAL(selectionChanged()),
                          this, SLOT(mapSelectionChange()),
                          Qt::UniqueConnection);
@@ -1029,6 +1026,12 @@ void MTextEditViewPrivate::handleDocumentUpdated()
         QTextOption option = maskedTextDocument->defaultTextOption();
         option.setTextDirection(dir);
         maskedTextDocument->setDefaultTextOption(option);
+    }
+
+    // New selection has to be notified because both changed text and changed formatting may
+    // change starting and ending position of selection.
+    if (!selectionOverlay.isNull()) {
+        mapSelectionChange();
     }
 }
 
@@ -1324,8 +1327,6 @@ void MTextEditViewPrivate::onSelectionOverlayVisibleChanged()
 {
     Q_Q(MTextEditView);
     if (!selectionOverlay.isNull() && !selectionOverlay.data()->isVisible()) {
-        QObject::disconnect(controller, SIGNAL(textChanged()),
-                            this, SLOT(mapSelectionChange()));
         QObject::disconnect(controller, SIGNAL(selectionChanged()),
                             this, SLOT(mapSelectionChange()));
         QObject::disconnect(MInputMethodState::instance(),
